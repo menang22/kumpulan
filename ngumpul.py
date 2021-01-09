703a5f11ae076203a9958bd9b72ed4e2c2ab2956
The Hackers 22 <76223089+menang22@users.noreply.github.com>
2021-01-09 11:03:43 +0700

    Create ngumpul.py

---
+++ ngumpul.py
@@ -0,0 +1,37 @@
+import requests, os
+
+def cekip():
+ print(f'[!] Mendapatkan IP..')
+ re = requests.get('https://api.myip.com').json()
+ ip = re['ip']
+ print(f'[!] IP kamu : {ip}')
+ 
+def iOsif():
+ print(f'[!] Menginstall tools osif..')
+ os.system('pkg update upgrade')
+ os.system('pkg install git python2')
+ os.system('git clone https://github.com/ciku370/OSIF')
+ os.system('cd OSIF')
+ os.system('pip2 install -r requirements.txt')
+ os.system('python2 osif.py')
+ 
+
+print('''-=[ MyTools ]=-
+
+     [Menu]
+[1] Cek IP
+[2] Install OSIF
+[3] Keluar
+''')
+menu = input('[?] Silahkan pilih menu : ')
+
+if menu == '1':
+ cekip()
+elif menu == '2':
+ iOsif()
+elif menu == '3':
+ print('[?] Keluar..')
+ sys.exit()
+else:
+ print('[?] Perintah tidak diketahui..')
+ sys.exit()
